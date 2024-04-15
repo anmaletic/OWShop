7b2c436fd7d9697ee2ada819ab17ee75c0cc84f6
AnMal <junkie_@msn.com>
2024-04-15 11:48:20 +0200

    Create ContactPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <div className="page">
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/contact" element={<ContactPage />} />
             <Route path="/login" element={<LoginPage />} />
           </Routes>
         </div>
---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,68 @@
+import { FormEvent, useState } from "react";
+import contactImg from "../assets/contact.jpeg";
+
+const ContactPage = () => {
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    try {
+      setMessage("Message sent!");
+    } catch (error) {
+      console.error("Error while sending a message:", error);
+    }
+  };
+
+  return (
+    <>
+      <div className="contact-page">
+        <form
+          className="col-lg-8 col-md-6 col-sm-8 col-8 "
+          onSubmit={handleSubmit}
+        >
+          <div className="contact-image mb-3">
+            <img src={contactImg} alt="Image" />
+          </div>
+
+          <div className="contact-user mb-3 d-flex gap-3">
+            <div className="flex-grow-1">
+              <label htmlFor="email" className="form-label">
+                Email address
+              </label>
+              <input type="email" className="form-control" id="email" />
+            </div>
+            <div className="flex-grow-1">
+              <label htmlFor="name" className="form-label">
+                Name
+              </label>
+              <input type="name" className="form-control" id="name" />
+            </div>
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="message" className="form-label">
+              Message
+            </label>
+            <textarea className="form-control" id="exampleInputPassword1" />
+          </div>
+
+          <div className="mb-3 d-flex">
+            <button type="submit" className="btn btn-success flex-grow-1">
+              Submit
+            </button>
+          </div>
+        </form>
+        <p>{message}</p>
+      </div>
+
+      <div className="footer">
+        <div>
+          <p>OWShop © 2024 OWShop. All rights reserved.</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContactPage;
