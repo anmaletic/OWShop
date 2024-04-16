bcfbcb226905625f51339fa5000f921f77eef559
AnMal <junkie_@msn.com>
2024-04-16 11:00:27 +0200

    Add validation to contact form

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,15 +1,42 @@
 import { FormEvent, useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import contactImg from "../assets/contact.jpeg";
 
-const ContactPage = () => {
+interface IFormInput {
+  email: string;
+  name: string;
+  message: string;
+}
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .required("Email is required")
+    .matches(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Email is not valid"
+    ),
+  name: yup.string().required("Name is required"),
+  message: yup.string().required("Message is required"),
+});
+
+const ContactPage: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>({ resolver: yupResolver(schema) });
+
   const [message, setMessage] = useState("");
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-
+  const onSubmit = async (data: IFormInput) => {
     try {
+      console.log(data);
       setMessage("Message sent!");
     } catch (error) {
+      setMessage("Error while sending a message!");
       console.error("Error while sending a message:", error);
     }
   };
@@ -19,7 +46,7 @@ const ContactPage = () => {
       <div className="contact-page">
         <form
           className="col-lg-8 col-md-6 col-sm-8 col-8 "
-          onSubmit={handleSubmit}
+          onSubmit={handleSubmit(onSubmit)}
         >
           <div className="contact-image mb-3">
             <img src={contactImg} alt="Image" />
@@ -30,13 +57,25 @@ const ContactPage = () => {
               <label htmlFor="email" className="form-label">
                 Email address
               </label>
-              <input type="email" className="form-control" id="email" />
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                {...register("email")}
+              />
+              {errors.email && <p className="error">{errors.email.message}</p>}
             </div>
             <div className="flex-grow-1">
               <label htmlFor="name" className="form-label">
                 Name
               </label>
-              <input type="name" className="form-control" id="name" />
+              <input
+                type="name"
+                className="form-control"
+                id="name"
+                {...register("name")}
+              />
+              {errors.name && <p className="error">{errors.name.message}</p>}
             </div>
           </div>
 
@@ -44,7 +83,14 @@ const ContactPage = () => {
             <label htmlFor="message" className="form-label">
               Message
             </label>
-            <textarea className="form-control" id="message" />
+            <textarea
+              className="form-control"
+              id="message"
+              {...register("message")}
+            />
+            {errors.message && (
+              <p className="error">{errors.message.message}</p>
+            )}
           </div>
 
           <div className="mb-3 d-flex">
