3019ba569b4c2856713aa7b7c9305b2c765fcc4d
AnMal <junkie_@msn.com>
2024-04-17 18:39:25 +0200

    Create API service

---
+++ src/services/apiService.ts
@@ -0,0 +1,33 @@
+export const login = async (username: string, password: string) => {
+    try {
+      const response = await fetch("https://fakestoreapi.com/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to login");
+      }
+      const data = await response.json();
+      return data.token;
+    } catch (error) {
+      console.error("Error during login:", error);
+      throw error;
+    }
+  };
+
+  export const fetchProducts = async () => {
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
+      if (!response.ok) {
+        throw new Error("Failed to fetch products");
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      throw error;
+    }
+  };
